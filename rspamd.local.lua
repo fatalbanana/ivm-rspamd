ee27eb67dc0e3f4df7f81e1c368d8c092d75ca9a
Andrew Lewis <nerf@judo.za.org>
2020-08-21 23:03:24 +0200

    Choose a better workaround

--- rspamd.local.lua
+++ rspamd.local.lua
@@ -1,8 +1,5 @@
 local rspamd_util = require 'rspamd_util'
 
--- For now we can't work if this rule is enabled :`(
-rspamd_config:add_condition('EMAIL_PLUS_ALIASES', function() return false end)
-
 local ivm_files = {
   '/etc/rspamd/ivm-sg-domains.lua',
   '/etc/rspamd/ivm-sg-id.lua',
