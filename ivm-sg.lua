0ca64bbdd3faa5a8dc71b6edc47a3d9a6815c239
Andrew Lewis <nerf@judo.za.org>
2020-10-03 18:13:30 +0200

    Remove workarounds for Rspamd < 2.6

--- ivm-sg.lua
+++ ivm-sg.lua
@@ -23,11 +23,10 @@ local cb_id = rspamd_config:register_symbol({
     if ivm_sendgrid_envfromdomains:get_key(env_from[1].domain) then
       task:insert_result('IVM_SENDGRID_DOMAIN', 1.0)
     end
-    -- Workaround for missing original SMTP from, FIXME some time
-    local tagged = task:get_symbol('TAGGED_FROM')
-    if not tagged then return end
-    local lp_re = rspamd_regexp.create_cached([[^(\d+)-]])
-    local res = lp_re:search(tagged[1].options[1], true, true)
+    local env_from = task:get_from{'smtp', 'orig'}
+    if not env_from then return end
+    local lp_re = rspamd_regexp.create_cached([[^bounces\+(\d+)-]])
+    local res = lp_re:search(env_from[1].user, true, true)
     if not res then return end
     if ivm_sendgrid_ids:get_key(res[1][2]) then
       task:insert_result('IVM_SENDGRID_ID', 1.0)
@@ -52,5 +51,3 @@ rspamd_config:register_symbol({
   score = 6.0,
   type = 'virtual',
 })
-
-rspamd_config:register_dependency('IVM_SENDGRID', 'TAGGED_FROM')
